2a4963d74da4539a4141b493338db1709268267f
Sarah Alsurayhi <sr.alsurayhi@gmail.com>
2024-10-19 21:25:16 +0300

    Initial Commit

---
+++ Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by E07 on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
